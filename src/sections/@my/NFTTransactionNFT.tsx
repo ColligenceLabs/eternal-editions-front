324ba5d83186c49322e01bb14b2890ae5a1fd51b
장종례 <chonglye@aartkorea.com>
2022-11-29 14:04:08 +0900

    no message

--- src/sections/@my/NFTTransactionNFT.tsx
+++ src/sections/@my/NFTTransactionNFT.tsx
@@ -11,7 +11,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import {styled} from "@mui/material/styles";
-import {CardActionArea, Stack} from "@mui/material";
+import {CardActionArea, Pagination, Stack} from "@mui/material";
 import {TableCellProps} from "@mui/material/TableCell/TableCell";
 import {TableRowProps} from "@mui/material/TableRow/TableRow";
 
@@ -69,38 +69,48 @@ export default function NFTTransactionNFT() {
     const {account} = useWallets();
 
     return <>
-        <TableContainer component={Paper} sx={{background: 'transparent'}}>
-            <Table sx={{minWidth: 650, background: 'transparent'}} aria-label="simple table">
-                <TableHead sx={{background: 'transparent'}}>
-                    <TableRow sx={{background: 'transparent', boxShadow: 'none'}}>
-                        <HeaderTableCell>Date</HeaderTableCell>
-                        <HeaderTableCell align="right">Type</HeaderTableCell>
-                        <HeaderTableCell align="right">NFT</HeaderTableCell>
-                        <HeaderTableCell align="right">Price</HeaderTableCell>
-                        <HeaderTableCell align="right">Chain</HeaderTableCell>
-                        <HeaderTableCell align="right">Confirmation</HeaderTableCell>
-                        <HeaderTableCell align="right">-</HeaderTableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <BodyTableRow
-                            key={row.confirmation}
-                        >
-                            <BodyTableCell>{row.date}</BodyTableCell>
-                            <BodyTableCell align="right">{row.type}</BodyTableCell>
-                            <BodyTableCell align="right">{row.nft}</BodyTableCell>
-                            <BodyTableCell align="right">{row.price}</BodyTableCell>
-                            <BodyTableCell align="right">{row.chain}</BodyTableCell>
-                            <BodyTableCell align="right">{row.confirmation}</BodyTableCell>
-                            <BodyTableCell align="right">
-                                -
-                            </BodyTableCell>
-                        </BodyTableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <Stack spacing={3}>
+            <Stack>
+                <TableContainer component={Paper} sx={{background: 'transparent'}}>
+                    <Table sx={{minWidth: 650, background: 'transparent'}} aria-label="simple table">
+                        <TableHead sx={{background: 'transparent'}}>
+                            <TableRow sx={{background: 'transparent', boxShadow: 'none'}}>
+                                <HeaderTableCell>Date</HeaderTableCell>
+                                <HeaderTableCell align="right">Type</HeaderTableCell>
+                                <HeaderTableCell align="right">NFT</HeaderTableCell>
+                                <HeaderTableCell align="right">Price</HeaderTableCell>
+                                <HeaderTableCell align="right">Chain</HeaderTableCell>
+                                <HeaderTableCell align="right">Confirmation</HeaderTableCell>
+                                <HeaderTableCell align="right">-</HeaderTableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map((row) => (
+                                <BodyTableRow
+                                    key={row.confirmation}
+                                >
+                                    <BodyTableCell>{row.date}</BodyTableCell>
+                                    <BodyTableCell align="right">{row.type}</BodyTableCell>
+                                    <BodyTableCell align="right">{row.nft}</BodyTableCell>
+                                    <BodyTableCell align="right">{row.price}</BodyTableCell>
+                                    <BodyTableCell align="right">{row.chain}</BodyTableCell>
+                                    <BodyTableCell align="right">{row.confirmation}</BodyTableCell>
+                                    <BodyTableCell align="right">
+                                        -
+                                    </BodyTableCell>
+                                </BodyTableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
 
+            </Stack>
+            <Stack direction="row"
+                   justifyContent="center"
+                   alignItems="center">
+
+                <Pagination count={4} variant="outlined" color="primary"/>
+            </Stack>
+        </Stack>
     </>
 }
