64238026d0376be422be81a58f7ada6b007a0c15
helloljho@gmail.com <helloljho@gmail.com>
2022-12-12 11:55:36 +0900

    add ticket list in landing page

--- src/sections/@home/HomeNowEvent.tsx
+++ src/sections/@home/HomeNowEvent.tsx
@@ -1,30 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // @mui
 import { styled } from '@mui/material/styles';
-import {Container } from '@mui/material';
+import { Container } from '@mui/material';
 // hooks
-import {useResponsive} from "../../hooks";
+import { useResponsive } from '../../hooks';
 // routes
 // components
-import {Swiper, SwiperSlide} from "swiper/react";
-import { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
 import PageHeader from '../../components/common/PageHeader';
 import Masonry from '@mui/lab/Masonry';
 import TicketPostItem from '../@eternaledtions/tickets/TicketPostItem';
-import {TicketProps} from '../../@types/ticket/ticket';
+import { TicketProps } from '../../@types/ticket/ticket';
 import TICKET from '../../sample/ticket';
 // styles
 import 'swiper/swiper.min.css';
-import "swiper/css/pagination";
-import TeamCard from "../@eternaledtions/main/TeamCard";
+import 'swiper/css/pagination';
+import TeamCard from '../@eternaledtions/main/TeamCard';
+import { getTicketsService } from '../../services/services';
+import { TicketInfoTypes } from '../../@types/ticket/ticketTypes';
 
 // ----------------------------------------------------------------------
 
-const RootStyle = styled('div')(({theme}) => ({
+const RootStyle = styled('div')(({ theme }) => ({
   [theme.breakpoints.up('md')]: {},
 }));
 
-const EventNowSection = styled('section')(({theme}) => ({
+const EventNowSection = styled('section')(({ theme }) => ({
   paddingTop: '32px',
   paddingBottom: '0',
   [theme.breakpoints.up('md')]: {
@@ -33,7 +35,7 @@ const EventNowSection = styled('section')(({theme}) => ({
 }));
 
 /* Swiper CSS Custom */
-const CustomSwiperBox = styled('div')(({theme}) => ({
+const CustomSwiperBox = styled('div')(({ theme }) => ({
   '.swiper': {
     paddingBottom: '30px',
   },
@@ -41,12 +43,12 @@ const CustomSwiperBox = styled('div')(({theme}) => ({
     bottom: 0,
   },
   '.swiper-pagination .swiper-pagination-bullet': {
-    opacity: .3,
+    opacity: 0.3,
     background: theme.palette.grey[0],
   },
   '.swiper-pagination .swiper-pagination-bullet-active': {
     opacity: 1,
-    background: theme.palette.primary.main
+    background: theme.palette.primary.main,
   },
 }));
 
@@ -55,28 +57,41 @@ const CustomSwiperBox = styled('div')(({theme}) => ({
 export default function HomeNowEvent() {
   const tickets: TicketProps[] = TICKET.tickets;
   const isDesktop = useResponsive('up', 'md');
+  const [ticketInfoList, setTicketInfoList] = useState<TicketInfoTypes[]>([]);
+
+  const getTickets = async () => {
+    const res = await getTicketsService(1, 6, '');
+    console.log(res);
+    if (res.status === 200) {
+      setTicketInfoList(res.data.list);
+    }
+  };
+
+  useEffect(() => {
+    getTickets();
+  }, []);
 
   return (
     <RootStyle>
       <EventNowSection>
-        <Container maxWidth={false} sx={{ px: { xs: '12px', lg: '80px' }}}>
-          <PageHeader title="NOW EVENTS" link='/tickets' />
+        <Container maxWidth={false} sx={{ px: { xs: '12px', lg: '80px' } }}>
+          <PageHeader title="NOW EVENTS" link="/tickets" />
           {isDesktop ? (
             <>
-            {tickets &&
-              <Masonry columns={{xs: 1, md: 2}} spacing={2}>
-                {tickets.map((ticket, index) => (
-                  <TicketPostItem key={index} ticket={ticket}/>
-                ))}
-              </Masonry>
-            }
+              {ticketInfoList && (
+                <Masonry columns={{ xs: 1, md: 2 }} spacing={2}>
+                  {ticketInfoList.map((ticket, index) => (
+                    <TicketPostItem key={index} ticket={ticket} />
+                  ))}
+                </Masonry>
+              )}
             </>
-            ) : (
+          ) : (
             <CustomSwiperBox>
               <Swiper pagination={true} modules={[Pagination]} spaceBetween={30}>
-                {tickets.map((ticket, index) => (
+                {ticketInfoList.map((ticket, index) => (
                   <SwiperSlide key={index}>
-                    <TicketPostItem key={index} ticket={ticket}/>
+                    <TicketPostItem key={index} ticket={ticket} />
                   </SwiperSlide>
                 ))}
               </Swiper>
--- src/services/services.ts
+++ src/services/services.ts
@@ -4,7 +4,7 @@ import env from '../env';
 const API_URL = `${env.REACT_APP_API_URL}/api`;
 
 export const getTicketsService = (page: number, perPage: number, category: string) => {
-  const url = `${API_URL}/service/collection/mysterybox/?&page=${page}&perPage=${perPage}&category=${category.toLowerCase()}`;
+  const url = `${API_URL}/service/collection/mysterybox/?&page=${page}&limit=${perPage}&category=${category.toLowerCase()}`;
   return axios.get(url);
 };
 
