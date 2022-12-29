8d3e3f5454cc9b310e68764f79633cff2f0e159b
helloljho@gmail.com <helloljho@gmail.com>
2022-12-29 17:26:09 +0900

    change api link

--- src/env.ts
+++ src/env.ts
@@ -7,19 +7,19 @@ type ENV = {
 const development: ENV = {
   REACT_APP_TARGET_NETWORK: 80001, // mumbai
   REACT_APP_TARGET_NETWORK_MSG: '',
-  REACT_APP_API_URL: 'http://127.0.0.1:5001',
+  REACT_APP_API_URL: 'https://dev.nftapi.eternaleditions.io',
 };
 
 const staging: ENV = {
   REACT_APP_TARGET_NETWORK: 80001, // mumbai
   REACT_APP_TARGET_NETWORK_MSG: '',
-  REACT_APP_API_URL: 'http://127.0.0.1:5001',
+  REACT_APP_API_URL: 'https://dev.nftapi.eternaleditions.io',
 };
 
 const production: ENV = {
   REACT_APP_TARGET_NETWORK: 137, // mainnet
   REACT_APP_TARGET_NETWORK_MSG: '',
-  REACT_APP_API_URL: 'http://127.0.0.1:5001',
+  REACT_APP_API_URL: 'https://dev.nftapi.eternaleditions.io',
 };
 
 const env =
