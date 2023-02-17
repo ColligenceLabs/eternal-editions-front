08833ce08392ea91c1f549426b1137c23eff8b89
helloljho@gmail.com <helloljho@gmail.com>
2023-02-17 10:11:50 +0900

    add kspay result

--- pages/kspay_result.tsx
+++ pages/kspay_result.tsx
@@ -1,31 +1,16 @@
-import React, { ChangeEvent, useRef } from 'react';
+import React from 'react';
 import { useState, useEffect, ReactElement } from 'react';
-// icons
-import menuIcon from '@iconify/icons-carbon/menu';
 // @mui
 import { styled } from '@mui/material/styles';
-import {
-  Box,
-  Button,
-  Container,
-  Input,
-  SelectChangeEvent,
-  Stack,
-  Table,
-  Typography,
-} from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 // config
 import { HEADER_MOBILE_HEIGHT, HEADER_DESKTOP_HEIGHT } from '../src/config';
-// _data
-import { _faqsSupport } from '../_data/mock';
 // layouts
 import Layout from '../src/layouts';
 // components
-import { Iconify, Page } from '../src/components';
-import { IconButtonAnimate } from '../src/components/animate';
+import { Page } from '../src/components';
 // sections
 import axios, { AxiosResponse } from 'axios';
-import iconv from 'iconv-lite';
 import { useRouter } from 'next/router';
 
 // ----------------------------------------------------------------------
@@ -39,38 +24,134 @@ const RootStyle = styled('div')(({ theme }) => ({
 
 // ----------------------------------------------------------------------
 
-const KSPAY_WEBHOST_URL = 'http://kspay.ksnet.to/store/KSPayWebV1.4/web_host/recv_post.jsp';
-const DEFAULT_DELIM = '`';
-const DEFAULT_RPARAMS =
-  'authyn`trno`trddt`trdtm`amt`authno`msg1`msg2`ordno`isscd`aqucd`result`resultcd';
-
 export default function KSPayResult() {
   const router = useRouter();
   console.log(router.query);
   const rcid = router.query['rcid'];
 
-  let payKey;
-  let typeStr;
+  const [payResult, setPayResult] = useState({
+    authyn: '',
+    trno: '',
+    trddt: '',
+    trdtm: '',
+    amt: '',
+    authno: '',
+    msg1: '',
+    msg2: '',
+    ordno: '',
+    isscd: '',
+    aqucd: '',
+    typeStr: '',
+    resultcd: '',
+  });
 
-  async function kspay_send_msg(rcid: any, mType: any) {
-    payKey = rcid;
+  async function kspay_send_msg(rcid: any) {
     const res: AxiosResponse = await axios.post('/api/ksnet/kspay_wh_result', { rcid });
     console.log(res.data);
     const rslt = res.data;
+    setPayResult({
+      authyn: rslt[1] === 'O' ? '승인성공' : '승인거절',
+      trno: rslt[2],
+      trddt: rslt[3],
+      trdtm: rslt[4],
+      amt: rslt[5],
+      authno: rslt[6],
+      msg1: rslt[7],
+      msg2: rslt[8],
+      ordno: rslt[9],
+      isscd: rslt[10],
+      aqucd: rslt[11],
+      typeStr:
+        rslt[12] === '1' || 'I'
+          ? '신용카드'
+          : rslt === '2'
+          ? '실시간계좌이체'
+          : rslt === '6'
+          ? '가상계좌발급'
+          : rslt === 'M'
+          ? '휴대폰결제'
+          : '(????)',
+      resultcd: rslt[13],
+    });
 
     return rslt;
   }
 
   useEffect(() => {
-    kspay_send_msg(rcid, '1');
-  }, []);
+    if (rcid) kspay_send_msg(rcid);
+  }, [rcid]);
+
+  useEffect(() => {
+    console.log(payResult);
+  }, [payResult]);
 
   return (
     <Page title="Support">
       <RootStyle>
         <Container>
           <Box sx={{ padding: '20px' }}>
-            <Typography variant={'h2'}>result</Typography>
+            <Typography variant={'h2'}>결제 결과</Typography>
+          </Box>
+          <Box sx={{ display: 'flex', gap: '1rem' }}>
+            <Typography variant={'h4'}>결제방법</Typography>
+            <Typography variant={'h4'}>{payResult.typeStr}</Typography>
+          </Box>
+          <Box sx={{ display: 'flex', gap: '1rem' }}>
+            <Typography variant={'h4'}>성공여부</Typography>
+            <Typography variant={'h4'}>{payResult.authyn}</Typography>
+          </Box>
+          <Box sx={{ display: 'flex', gap: '1rem' }}>
+            <Typography variant={'h4'}>응답코드</Typography>
+            <Typography variant={'h4'}>{payResult.resultcd}</Typography>
+          </Box>
+          <Box sx={{ display: 'flex', gap: '1rem' }}>
+            <Typography variant={'h4'}>주문번호</Typography>
+            <Typography variant={'h4'}>{payResult.ordno}</Typography>
+          </Box>
+          <Box sx={{ display: 'flex', gap: '1rem' }}>
+            <Typography variant={'h4'}>금액</Typography>
+            <Typography variant={'h4'}>{payResult.amt}</Typography>
+          </Box>
+          <Box sx={{ display: 'flex', gap: '1rem' }}>
+            <Typography variant={'h4'}>거래번호</Typography>
+            <Typography variant={'h4'}>{payResult.trno}</Typography>
+          </Box>
+          <Box sx={{ display: 'flex', gap: '1rem' }}>
+            <Typography variant={'h4'}>거래일자</Typography>
+            <Typography variant={'h4'}>{payResult.trddt}</Typography>
+          </Box>
+          <Box sx={{ display: 'flex', gap: '1rem' }}>
+            <Typography variant={'h4'}>거래시간</Typography>
+            <Typography variant={'h4'}>{payResult.trdtm}</Typography>
+          </Box>
+
+          {payResult.authyn && 'O' === payResult.authyn && (
+            <Box sx={{ display: 'flex', gap: '1rem' }}>
+              <Typography variant={'h4'}>카드사 승인번호/은행 코드번호</Typography>
+              <Typography
+                variant={'h4'}
+              >{`${payResult.authno} :카드사에서 부여한 번호로 고유한값은 아닙니다.`}</Typography>
+            </Box>
+          )}
+
+          <Box sx={{ display: 'flex', gap: '1rem' }}>
+            <Typography variant={'h4'}>발급사코드/가상계좌번호/계좌이체번호</Typography>
+            <Typography variant={'h4'}>{payResult.isscd}</Typography>
+          </Box>
+
+          <Box sx={{ display: 'flex', gap: '1rem' }}>
+            <Typography variant={'h4'}>매입사코드</Typography>
+            <Typography variant={'h4'}>{payResult.aqucd}</Typography>
+          </Box>
+
+          <Box sx={{ display: 'flex', gap: '1rem' }}>
+            <Typography variant={'h4'}>메시지1</Typography>
+            <Typography variant={'h4'}>{payResult.msg1}</Typography>
+          </Box>
+
+          <Box sx={{ display: 'flex', gap: '1rem' }}>
+            <Typography variant={'h4'}>메시지2</Typography>
+            <Typography variant={'h4'}>{payResult.msg2}</Typography>
           </Box>
         </Container>
       </RootStyle>
