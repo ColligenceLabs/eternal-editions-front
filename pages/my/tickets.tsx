cb160864ea51897a4495444d6f419538227f4000
helloljho@gmail.com <helloljho@gmail.com>
2023-03-06 13:38:16 +0900

    change popup menu

--- pages/my/tickets.tsx
+++ pages/my/tickets.tsx
@@ -1,59 +1,53 @@
-import {ReactElement, useEffect, useState} from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 // utils
 // @types
 // _data
 // layouts
 import Layout from '../../src/layouts';
 // components
-import {Page} from '../../src/components';
-import {Container, Stack, Typography} from "@mui/material";
-import {styled} from "@mui/material/styles";
-import {HEADER_DESKTOP_HEIGHT, HEADER_MOBILE_HEIGHT} from "../../src/config";
-import MyTicketList from "../../src/sections/@my/MyTicketList";
-import TICKET from "../../src/sample/ticket";
-import PageHeader from "../../src/components/common/PageHeader";
+import { Page } from '../../src/components';
+import { Container, Stack, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { HEADER_DESKTOP_HEIGHT, HEADER_MOBILE_HEIGHT } from '../../src/config';
+import MyTicketList from '../../src/sections/@my/MyTicketList';
+import TICKET from '../../src/sample/ticket';
+import PageHeader from '../../src/components/common/PageHeader';
 // sections
 
 // ----------------------------------------------------------------------
-const RootStyle = styled('div')(({theme}) => ({
-    paddingTop: HEADER_MOBILE_HEIGHT,
-    [theme.breakpoints.up('md')]: {
-        paddingTop: HEADER_DESKTOP_HEIGHT,
-    },
+const RootStyle = styled('div')(({ theme }) => ({
+  paddingTop: HEADER_MOBILE_HEIGHT,
+  [theme.breakpoints.up('md')]: {
+    paddingTop: HEADER_DESKTOP_HEIGHT,
+  },
 }));
-
 
 type Props = {};
 
 export default function MyTicketPage({}: Props) {
+  // const [tickets, setTickets] = useState([]);
 
-    // const [tickets, setTickets] = useState([]);
+  useEffect(() => {
+    // setTickets();
+  }, []);
 
-    useEffect(() => {
-        // setTickets();
-    }, []);
+  return (
+    <Page title="Account">
+      <RootStyle>
+        <Container sx={{ mt: 3 }}>
+          <PageHeader title="My Ticket" />
 
-    return (
-        <Page title="Account">
-            <RootStyle>
-                <Container sx={{mt:3}}>
-
-                    <PageHeader title="My Ticket"/>
-
-
-                    <Stack sx={{mb: 3}}>
-
-                        <MyTicketList loading={false} tickets={TICKET.myTickets}/>
-
-                    </Stack>
-                </Container>
-            </RootStyle>
-        </Page>
-    );
+          <Stack sx={{ mb: 3 }}>
+            <MyTicketList loading={false} tickets={TICKET.myTickets} />
+          </Stack>
+        </Container>
+      </RootStyle>
+    </Page>
+  );
 }
 
 // ----------------------------------------------------------------------
 
 MyTicketPage.getLayout = function getLayout(page: ReactElement) {
-    return <Layout>{page}</Layout>;
+  return <Layout>{page}</Layout>;
 };
