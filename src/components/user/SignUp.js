1b4a03cefb9558f9a7f18cf8fca18eff29ac020e
helloljho@gmail.com <helloljho@gmail.com>
2023-03-22 09:44:32 +0900

    fix typo

--- src/components/user/SignUp.js
+++ src/components/user/SignUp.js
@@ -347,7 +347,7 @@ export default function SignUp({ onClose, hideSns, ...other }) {
                     }}
                     // startIcon={<Iconify icon={'mdi:facebook'} />}
                   >
-                    ID / PASSWORD (신규회원)
+                    ID / PASSWORD (기존회원)
                   </IDPWDButton>
                 </Stack>
                 <Stack direction="row" justifyContent="center" alignItems="center">
