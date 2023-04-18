320c775384cdfeffe4fd71d1e369c0c539f2014f
YoonYIgeon <whaledoll@naver.com>
2023-04-18 15:48:50 +0900

    updated font

--- src/sections/@about/AboutHeader.tsx
+++ src/sections/@about/AboutHeader.tsx
@@ -1,83 +1,86 @@
 // icons
 // @mui
-import {styled} from '@mui/material/styles';
-import {Box, Container} from '@mui/material';
-import React, {useEffect, useState} from "react";
+import { styled } from '@mui/material/styles';
+import { Box, Container } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 // hooks
-import {useTheme} from "@mui/system";
-import moment from "moment/moment";
+import { useTheme } from '@mui/system';
+import moment from 'moment/moment';
 // routes
 // components
 
 // ----------------------------------------------------------------------
 
-const RootStyle = styled('div')(({theme}) => ({
-    [theme.breakpoints.up('md')]: {},
+const RootStyle = styled('div')(({ theme }) => ({
+  [theme.breakpoints.up('md')]: {},
 }));
 
-const AboutHeaderSection = styled('section')(({theme}) => ({
-    position: 'relative',
-    paddingTop: '70px',
-    paddingBottom: '70px',
-    overflow: 'hidden',
-    [theme.breakpoints.up('md')]: {
-        paddingTop: '130px',
-        paddingBottom: '100px',
-    },
+const AboutHeaderSection = styled('section')(({ theme }) => ({
+  position: 'relative',
+  paddingTop: '70px',
+  paddingBottom: '70px',
+  overflow: 'hidden',
+  [theme.breakpoints.up('md')]: {
+    paddingTop: '130px',
+    paddingBottom: '100px',
+  },
 }));
 
 // ----------------------------------------------------------------------
 
 export default function AboutHeader() {
-    const theme = useTheme();
-    const [time, setTime] = useState<moment.Moment>(moment());
-    const [dateStr, setDateStr] = useState<string>('');
-    const [timeStr, setTimeStr] = useState<string>('');
+  const theme = useTheme();
+  const [time, setTime] = useState<moment.Moment>(moment());
+  const [dateStr, setDateStr] = useState<string>('');
+  const [timeStr, setTimeStr] = useState<string>('');
 
-    useEffect(() => {
-        setInterval(() => {
-            setTime(moment());
-        }, 1000);
-    }, []);
+  useEffect(() => {
+    setInterval(() => {
+      setTime(moment());
+    }, 1000);
+  }, []);
 
-    useEffect(() => {
-        setDateStr(time.format('YYYY.MM.DD'));
-        setTimeStr(time.format('HH:mm:ss'));
-    }, [time]);
+  useEffect(() => {
+    setDateStr(time.format('YYYY.MM.DD'));
+    setTimeStr(time.format('HH:mm:ss'));
+  }, [time]);
 
-    return (
-        <RootStyle>
-            <AboutHeaderSection>
-                <Container maxWidth={false} sx={{px: {xs: '12px', lg: '80px'}}}>
-                    <Box
-                        component="h1"
-                        sx={{
-                            typography: 'banner1',
-                            color: theme.palette.primary.main,
-                            mb: {xs: '16px', lg: '40px'},
-                        }}
-                    >
-                        ETERNAL<br />
-                        EDITIONS<br />
-                        {dateStr}<br />
-                        {timeStr}
-                    </Box>
-                    <Box
-                        component="p"
-                        sx={{
-                            maxWidth: {lg: '768px'},
-                            fontSize: {xs: '20px', lg: '40px'},
-                            fontWeight: 'bold',
-                            lineHeight: {xs: '26px', lg: '44px'},
-                            textTransform: 'uppercase',
-                        }}
-                    >
-                        Eternal Editions gives new values in the live event industry.In the WEB 3.0 world, live event
-                        planners and participants communicate, BUIDL up, and share together. Our mission is to make all
-                        those experiences fun and convenient
-                    </Box>
-                </Container>
-            </AboutHeaderSection>
-        </RootStyle>
-    );
+  return (
+    <RootStyle>
+      <AboutHeaderSection>
+        <Container maxWidth={false} sx={{ px: { xs: '12px', lg: '80px' } }}>
+          <Box
+            component="h1"
+            sx={{
+              typography: 'banner1',
+              color: theme.palette.primary.main,
+              mb: { xs: '16px', lg: '40px' },
+            }}
+          >
+            ETER
+            <br />
+            EDITIONS
+            <br />
+            {dateStr}
+            <br />
+            {timeStr}
+          </Box>
+          <Box
+            component="p"
+            sx={{
+              maxWidth: { lg: '768px' },
+              fontSize: { xs: '20px', lg: '40px' },
+              fontWeight: 'bold',
+              lineHeight: { xs: '26px', lg: '44px' },
+              textTransform: 'uppercase',
+            }}
+          >
+            Eternal Editions gives new values in the live event industry.In the WEB 3.0 world, live
+            event planners and participants communicate, BUIDL up, and share together. Our mission
+            is to make all those experiences fun and convenient
+          </Box>
+        </Container>
+      </AboutHeaderSection>
+    </RootStyle>
+  );
 }
--- src/sections/@home/HomeEternalEditions.tsx
+++ src/sections/@home/HomeEternalEditions.tsx
@@ -1,22 +1,22 @@
 // icons
 // @mui
-import {styled} from '@mui/material/styles';
-import {Box, Container} from '@mui/material';
-import React, {useEffect, useState} from "react";
-import {useTheme} from "@mui/system";
+import { styled } from '@mui/material/styles';
+import { Box, Container } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { useTheme } from '@mui/system';
 // hooks
-import PageHeader from "../../components/common/PageHeader";
+import PageHeader from '../../components/common/PageHeader';
 // routes
 // components
 import moment from 'moment';
 
 // ----------------------------------------------------------------------
 
-const RootStyle = styled('div')(({theme}) => ({
+const RootStyle = styled('div')(({ theme }) => ({
   [theme.breakpoints.up('md')]: {},
 }));
 
-const EternalEditionSection = styled('section')(({theme}) => ({
+const EternalEditionSection = styled('section')(({ theme }) => ({
   position: 'relative',
   paddingTop: '454px',
   paddingBottom: '160px',
@@ -30,7 +30,7 @@ const EternalEditionBody = styled('div')(() => ({
   position: 'relative',
 }));
 
-const ImgHuman = styled('img')(({theme}) => ({
+const ImgHuman = styled('img')(({ theme }) => ({
   position: 'absolute',
   top: '90px',
   right: '-45%',
@@ -56,47 +56,52 @@ export default function HomeEternalEditions() {
     }, 1000);
   }, []);
 
-  useEffect( () => {
-      setDateStr(time.format('YYYY.MM.DD'));
-      setTimeStr(time.format('HH:mm:ss'));
+  useEffect(() => {
+    setDateStr(time.format('YYYY.MM.DD'));
+    setTimeStr(time.format('HH:mm:ss'));
   }, [time]);
 
   return (
-      <RootStyle>
-        <EternalEditionSection>
-          <Container maxWidth={false} sx={{ px: { xs: '12px', lg: '80px' }}}>
-            <ImgHuman src="/assets/img/img-human.png" />
+    <RootStyle>
+      <EternalEditionSection>
+        <Container maxWidth={false} sx={{ px: { xs: '12px', lg: '80px' } }}>
+          <ImgHuman src="/assets/img/img-human.png" />
 
-            <EternalEditionBody>
-              <PageHeader title="ABOUT" link='/about-us' />
-              <Box
-                  component="h1"
-                  sx={{
-                    typography: 'banner1',
-                    color: theme.palette.primary.main,
-                    mb: { xs: '16px', lg: '40px' },
-                  }}
-              >
-                ETERNAL<br />
-                EDITIONS<br />
-                {dateStr}<br />
-                {timeStr}
-              </Box>
-              <Box
-                  component="p"
-                  sx={{
-                    maxWidth: { lg: '768px' },
-                    fontSize: { xs: '20px', lg: '40px'},
-                    fontWeight: 'bold',
-                    lineHeight: { xs: '26px', lg: '44px' },
-                    textTransform: 'uppercase',
-                  }}
-              >
-                Eternal Editions gives new values in the live event industry.In the WEB 3.0 world, live event planners and participants communicate, BUIDL up, and share together. Our mission is to make all those experiences fun and convenient
-              </Box>
-            </EternalEditionBody>
-          </Container>
-        </EternalEditionSection>
-      </RootStyle>
+          <EternalEditionBody>
+            <PageHeader title="ABOUT" link="/about-us" />
+            <Box
+              component="h1"
+              sx={{
+                typography: 'banner1',
+                color: theme.palette.primary.main,
+                mb: { xs: '16px', lg: '40px' },
+              }}
+            >
+              ETERNAL
+              <br />
+              EDITIONS
+              <br />
+              {dateStr}
+              <br />
+              {timeStr}
+            </Box>
+            <Box
+              component="p"
+              sx={{
+                maxWidth: { lg: '768px' },
+                fontSize: { xs: '20px', lg: '40px' },
+                fontWeight: 'bold',
+                lineHeight: { xs: '26px', lg: '44px' },
+                textTransform: 'uppercase',
+              }}
+            >
+              Eternal Editions gives new values in the live event industry.In the WEB 3.0 world,
+              live event planners and participants communicate, BUIDL up, and share together. Our
+              mission is to make all those experiences fun and convenient
+            </Box>
+          </EternalEditionBody>
+        </Container>
+      </EternalEditionSection>
+    </RootStyle>
   );
 }
