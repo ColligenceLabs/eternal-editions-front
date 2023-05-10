44b86fb002dd0ff16730250992a98bbcf87a5954
Mai Doan <maidt@chainos.vn>
2023-05-10 20:32:09 +0700

    feat: update style on mobile

--- pages/my/tickets/sell.tsx
+++ pages/my/tickets/sell.tsx
@@ -3,13 +3,26 @@ import Layout from 'src/layouts';
 import { Page } from 'src/components';
 import { Container } from '@mui/material';
 import MyTicketSell from 'src/sections/@my/MyTicketSell';
+import { HEADER_DESKTOP_HEIGHT, HEADER_MOBILE_HEIGHT } from 'src/config';
 
 // ----------------------------------------------------------------------
 
 export default function MyTicketSellPage() {
   return (
     <Page title="Sell">
-      <Container sx={{ py: '45px', height: '100vh' }}>
+      <Container
+        sx={{
+          height: '100vh',
+          paddingTop: {
+            xs: `${HEADER_MOBILE_HEIGHT}px`,
+            md: `${HEADER_DESKTOP_HEIGHT}px`,
+          },
+          paddingBottom: {
+            xs: '32px',
+            md: '45px',
+          },
+        }}
+      >
         <MyTicketSell />
       </Container>
     </Page>
@@ -28,7 +41,6 @@ MyTicketSellPage.getLayout = function getLayout(page: ReactElement) {
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
       }}
-      disabledHeader
       disabledFooter
     >
       {page}
--- src/sections/@my/MyTicketSell.tsx
+++ src/sections/@my/MyTicketSell.tsx
@@ -16,6 +16,7 @@ import {
   Stack,
   styled,
   Typography,
+  useTheme,
 } from '@mui/material';
 import React, { useState } from 'react';
 import { EEAvatar } from 'src/components';
@@ -74,11 +75,15 @@ const FootText = styled(Typography)({
   color: 'rgba(255, 255, 255, 0.6)',
 });
 
-const Row = styled(Stack)({
-  flexDirection: 'row',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-});
+const Row = styled(Stack)(({ theme }) => ({
+  flexDirection: 'column',
+  gap: 8,
+  [theme.breakpoints.up('md')]: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+}));
 
 const Section = styled(Stack)({
   gap: '12px',
@@ -86,8 +91,12 @@ const Section = styled(Stack)({
 
 const StyledButton = styled(Button)(({ theme }) => ({
   fontWeight: theme.typography.fontWeightBold,
-  fontSize: 14,
-  lineHeight: 12 / 14,
+  fontSize: 12,
+  lineHeight: 13 / 12,
+  [theme.breakpoints.up('md')]: {
+    fontSize: 14,
+    lineHeight: 12 / 14,
+  },
   letterSpacing: '0.08em',
   textTransform: 'uppercase',
   color: theme.palette.common.black,
@@ -98,12 +107,17 @@ const StyledButton = styled(Button)(({ theme }) => ({
 }));
 
 const StyledSelect = styled(Select)(({ theme }) => ({
-  fontSize: '14px',
-  letterSpacing: '0.08em',
   textTransform: 'uppercase',
   textAlign: 'center',
   borderRadius: '60px',
   minHeight: 'unset',
+  letterSpacing: '0.08em',
+  fontSize: 12,
+  lineHeight: 13 / 12,
+  [theme.breakpoints.up('md')]: {
+    fontSize: 14,
+    lineHeight: 12 / 14,
+  },
   [`& .${selectClasses.icon}`]: {
     right: '16px',
     top: '50%',
@@ -114,12 +128,17 @@ const StyledSelect = styled(Select)(({ theme }) => ({
     borderColor: theme.palette.common.white,
   },
   [`& .${selectClasses.select}`]: {
-    padding: '22px 40px',
+    paddingTop: '11px',
+    paddingBottom: '11px',
     fontWeight: 'bold',
     color: theme.palette.common.white,
     borderRadius: 'inherit',
     height: 'unset',
     minHeight: 'unset',
+    whiteSpace: 'pre-wrap',
+    [theme.breakpoints.up('md')]: {
+      padding: '22px 40px',
+    },
   },
   [`&:hover, &.Mui-focused`]: {
     [`.${outlinedInputClasses.notchedOutline}`]: {
@@ -196,14 +215,15 @@ const MenuProps = {
 
 const Hr = styled('hr')({
   width: '100%',
-  borderWidth: '1px',
-  borderColor: 'rgba(255, 255, 255, 0.4)',
+  border: 'none',
+  borderBottom: '1px solid rgba(255, 255, 255, 0.4)',
 });
 
 // ----------------------------------------------------------------------
 
 export default function MyTicketSell() {
   const { data } = useRequest(`api/service/mysterybox/336`);
+  const theme = useTheme();
   const ticketInfo = data?.data;
   const [typeOfSale, setTypeOfSale] = useState(TYPES_OF_SALE[0].value);
   const [priceUnit, setPriceUnit] = useState(PRICE_UNITS[0].value);
@@ -226,167 +246,189 @@ export default function MyTicketSell() {
   };
 
   return (
-    <Stack
-      gap={3}
+    <Box
       sx={{
         background: 'rgba(0, 0, 0, 0.24)',
         backdropFilter: 'blur(50px)',
         borderRadius: '40px',
         width: 'min(100%, 400px)',
         height: '100%',
-        overflowY: 'auto',
-        padding: 3,
+        paddingTop: 3,
+        paddingBottom: 3,
         marginLeft: 'auto',
-        [`&::-webkit-scrollbar`]: {
-          width: '7px',
-          borderRadius: '40px',
-        },
-        [`&::-webkit-scrollbar-track`]: {
-          marginTop: '24px',
-          marginBottom: '24px',
-        },
-        [`&:hover::-webkit-scrollbar-thumb`]: {
-          background: 'rgba(0, 0, 0, 0.12)',
-          borderRadius: '40px',
-        },
       }}
     >
       <Stack
-        direction="row"
-        spacing={0.5}
-        alignItems="center"
-        sx={{ opacity: 0.72, typography: 'caption' }}
+        gap={3}
+        sx={{
+          height: '100%',
+          paddingLeft: 3,
+          paddingRight: 3,
+          overflowY: 'auto',
+          [`&::-webkit-scrollbar`]: {
+            width: '7px',
+            borderRadius: '40px',
+          },
+          [`&:hover::-webkit-scrollbar-thumb`]: {
+            background: 'rgba(0, 0, 0, 0.12)',
+            borderRadius: '40px',
+          },
+        }}
       >
-        <EEAvatar
-          account={'0x8B7B2b4F7A391b6f14A81221AE0920a9735B67Fc'}
-          image={'url(/assets/static/avatars/1.jpg)'}
-          nickname={'by @iloveseoul'}
-          sx={{ mr: 0, width: 24, height: 24 }}
-        />
-
-        <Typography fontSize={14} lineHeight={20 / 14}>
-          by @iloveseoul
+        <Stack
+          direction="row"
+          spacing={0.5}
+          alignItems="center"
+          sx={{
+            opacity: 0.72,
+            typography: 'caption',
+            [theme.breakpoints.down('md')]: {
+              marginBottom: -1,
+            },
+          }}
+        >
+          <EEAvatar
+            account={'0x8B7B2b4F7A391b6f14A81221AE0920a9735B67Fc'}
+            image={'url(/assets/static/avatars/1.jpg)'}
+            nickname={'by @iloveseoul'}
+            sx={{ mr: 0, width: 24, height: 24 }}
+          />
+          <Typography fontSize={14} lineHeight={20 / 14}>
+            by @iloveseoul
+          </Typography>
+        </Stack>
+        <Typography
+          sx={{
+            fontWeight: 'bold',
+            fontSize: 24,
+            lineHeight: 28 / 24,
+            [theme.breakpoints.up('md')]: {
+              fontSize: 40,
+              lineHeight: 44 / 40,
+            },
+          }}
+        >
+          DCENTRAL Miami 2023 VIP
         </Typography>
-      </Stack>
 
-      <Typography variant="h2">DCENTRAL Miami 2023 VIP</Typography>
-
-      <Box>
-        <Row>
-          <Label>Day</Label>
-          <Value>Wednesday (November 11,2023)</Value>
-        </Row>
-        <Row>
-          <Label>Team</Label>
-          <Value>Team Yellow</Value>
-        </Row>
-      </Box>
-
-      <Section>
-        <Label>CHOOSE A TYPE OF SALE</Label>
-        <StyledSelect
-          fullWidth
-          variant="outlined"
-          value={typeOfSale}
-          onChange={onChangeTypeOfSale}
-          MenuProps={MenuProps}
-        >
-          {TYPES_OF_SALE.map((option) => (
-            <StyledMenuItem key={option.value} value={option.value} disableGutters>
-              {option.label}
-            </StyledMenuItem>
-          ))}
-        </StyledSelect>
-      </Section>
-
-      <Box>
-        <Label>SET A PRICE</Label>
-        <FormControl variant="standard" fullWidth>
-          <StyledInput
-            placeholder="Amount"
-            endAdornment={
-              <InputAdornment position="end">
-                <StyledSelect
-                  size="small"
-                  value={priceUnit}
-                  onChange={onChangeUnit}
-                  MenuProps={MenuProps}
-                >
-                  {PRICE_UNITS.map((unit) => (
-                    <StyledMenuItem key={unit.value} value={unit.value}>
-                      {unit.label}
-                    </StyledMenuItem>
-                  ))}
-                </StyledSelect>
-              </InputAdornment>
-            }
-          />
-        </FormControl>
-      </Box>
-
-      <Section>
-        <Label>DURATION</Label>
-        <StyledSelect
-          fullWidth
-          variant="outlined"
-          value={duration}
-          onChange={onChangeDuration}
-          MenuProps={MenuProps}
-        >
-          {DURATIONS.map((option) => (
-            <StyledMenuItem key={option.value} value={option.value} disableGutters>
-              {option.label}
-            </StyledMenuItem>
-          ))}
-        </StyledSelect>
-      </Section>
-
-      <Box>
-        <Label>SET A PRICE</Label>
-        <FormControl variant="standard" fullWidth>
-          <StyledInput
-            placeholder="Amount"
-            endAdornment={
-              <InputAdornment position="end">
-                <Typography color="white" fontWeight="bold" fontSize={14} lineHeight={12 / 14}>
-                  %
-                </Typography>
-              </InputAdornment>
-            }
-          />
-        </FormControl>
-        <FootText mt="12px">
-          Creator earning are optional for this collection. You can give them up to 7.50% of your
-          sale.
-        </FootText>
-      </Box>
-
-      <Section>
-        <Label>Summary</Label>
-        <Stack gap="7px">
+        <Stack gap={{ xs: 2, md: 0 }}>
           <Row>
-            <Label>Listing Price</Label>
-            <Value>-- EDCP</Value>
+            <Label>Day</Label>
+            <Value>Wednesday (November 11,2023)</Value>
           </Row>
           <Row>
-            <Label>Service fee</Label>
-            <Value>2.5%</Value>
-          </Row>{' '}
-          <Row>
-            <Label>Creator earnings</Label>
-            <Value>7.5%</Value>
+            <Label>Team</Label>
+            <Value>Team Yellow</Value>
           </Row>
         </Stack>
-      </Section>
 
-      <Hr sx={{ my: '-12px' }} />
+        <Section>
+          <Label>CHOOSE A TYPE OF SALE</Label>
+          <StyledSelect
+            fullWidth
+            variant="outlined"
+            value={typeOfSale}
+            onChange={onChangeTypeOfSale}
+            MenuProps={MenuProps}
+          >
+            {TYPES_OF_SALE.map((option) => (
+              <StyledMenuItem key={option.value} value={option.value} disableGutters>
+                {option.label}
+              </StyledMenuItem>
+            ))}
+          </StyledSelect>
+        </Section>
 
-      <Row>
-        <Label>Potential earning</Label>
-        <TotalValue>-- EDCP</TotalValue>
-      </Row>
+        <Hr sx={{ [theme.breakpoints.up('md')]: { display: 'none' } }} />
 
-      <StyledButton>COMPLETE LISTING</StyledButton>
-    </Stack>
+        <Box>
+          <Label>SET A PRICE</Label>
+          <FormControl variant="standard" fullWidth>
+            <StyledInput
+              placeholder="Amount"
+              endAdornment={
+                <InputAdornment position="end">
+                  <StyledSelect
+                    size="small"
+                    value={priceUnit}
+                    onChange={onChangeUnit}
+                    MenuProps={MenuProps}
+                  >
+                    {PRICE_UNITS.map((unit) => (
+                      <StyledMenuItem key={unit.value} value={unit.value}>
+                        {unit.label}
+                      </StyledMenuItem>
+                    ))}
+                  </StyledSelect>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+        </Box>
+
+        <Section>
+          <Label>DURATION</Label>
+          <StyledSelect
+            fullWidth
+            variant="outlined"
+            value={duration}
+            onChange={onChangeDuration}
+            MenuProps={MenuProps}
+          >
+            {DURATIONS.map((option) => (
+              <StyledMenuItem key={option.value} value={option.value} disableGutters>
+                {option.label}
+              </StyledMenuItem>
+            ))}
+          </StyledSelect>
+        </Section>
+
+        <Box>
+          <Label>SET A PRICE</Label>
+          <FormControl variant="standard" fullWidth>
+            <StyledInput
+              endAdornment={
+                <InputAdornment position="end">
+                  <Typography color="white" fontWeight="bold" fontSize={14} lineHeight={12 / 14}>
+                    %
+                  </Typography>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+          <FootText mt="12px">
+            Creator earning are optional for this collection. You can give them up to 7.50% of your
+            sale.
+          </FootText>
+        </Box>
+
+        <Section>
+          <Label>Summary</Label>
+          <Stack gap="7px">
+            <Row>
+              <Label>Listing Price</Label>
+              <Value>-- EDCP</Value>
+            </Row>
+            <Row>
+              <Label>Service fee</Label>
+              <Value>2.5%</Value>
+            </Row>{' '}
+            <Row>
+              <Label>Creator earnings</Label>
+              <Value>7.5%</Value>
+            </Row>
+          </Stack>
+        </Section>
+
+        <Hr sx={{ my: '-12px' }} />
+
+        <Row>
+          <Label>Potential earning</Label>
+          <TotalValue>-- EDCP</TotalValue>
+        </Row>
+
+        <StyledButton>COMPLETE LISTING</StyledButton>
+      </Stack>
+    </Box>
   );
 }
