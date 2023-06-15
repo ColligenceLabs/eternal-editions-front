04af50b917efc2d148b052e123cde4a6edfd4107
helloljho@gmail.com <helloljho@gmail.com>
2023-06-15 17:11:57 +0900

    change bank account info

--- pages/my/account.tsx
+++ pages/my/account.tsx
@@ -666,7 +666,9 @@ Type: Address verification`;
 
                     <Stack gap="12px">
                       <SectionHeader>bank Account</SectionHeader>
-                      {bankAccount ? <SectionText>{bankAccount.accountNumber}</SectionText> : null}
+                      {bankAccount ? (
+                        <SectionText>{`${bankAccount.bank} ${bankAccount.accountNumber}`}</SectionText>
+                      ) : null}
                       <Stack flexDirection="row">
                         <CButton
                           onClick={() => {
